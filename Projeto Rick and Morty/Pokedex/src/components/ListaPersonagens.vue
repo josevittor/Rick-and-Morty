827a4dbde25e8451585f045e667fbd77b5e9c555
josevittor <129238767+josevittor@users.noreply.github.com>
2024-05-03 20:51:51 -0300

    Add files via upload

---
+++ Projeto Rick and Morty/Pokedex/src/components/ListaPersonagens.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+
+defineProps([
+    "personagem"
+])
+
+</script>
+
+<template>
+    <div class="col">
+        <div class="card m-3">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img :src="personagem.image" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ personagem.name }}</h5>
+                        <p class="card-text">
+                            Status: {{ personagem.status }} <br>
+                            Espécie: {{ personagem.species }} <br>
+                            Gênero: {{ personagem.gender }} <br>
+                            Local: {{ personagem.location.name }} <br>
+                            Total De episódios: {{ personagem.episode.length }} <br>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ Projeto Rick and Morty/Pokedex/src/views/AboutView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h2>Nome: José Víttor Raymundo Guarido</h2>
+    <h2>RA: 1963254</h2>
+    <h2>Curso: Análise e Desenvolvimento de Sistemas</h2>
+    <h2>Universidade: Unimar</h2>
+  </div>
+</template>
